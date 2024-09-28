aec6c8ed442dd8d7261828532dbec317abf59d97
LuisMy1715Y <hernandezsuarezluismiguel@gmail.com>
2024-09-28 01:35:19 -0500

    porfolio.web

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://LuisMy1715Y.github.io',
+  base: 'porfolio.web',
 });
 
